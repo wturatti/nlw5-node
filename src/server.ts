c138bc5deb22fd7b36204736741dfc637b74c27c
William Turatti <wturatti@gmail.com>
2021-04-20 14:02:42 -0300

    Segundo dia nlw5.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-app.get("/", (request, response) =>{
-  return response.json({ message: "Olá #NLW5" });
-});
+app.use(express.json());
 
-app.post("/users", (request, response) => {
-  return response.json({ message: "Usuário salvo com sucesso" })
-});
+app.use(routes);
 
-app.listen(3333, () => console.log("Run on port 3333")); 
+app.listen(3333, () => console.log("Server running on port 3333")); 
